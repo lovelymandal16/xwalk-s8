dc4fe431fc6694c00271f5075e7918e1646f88b5
Lovely Mandal <155715221+lovelymandal16@users.noreply.github.com>
2025-11-10 11:17:53 +0530

    Update form-editor-support.js

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -352,10 +352,10 @@ function enableAuthoringAssistantExtension() {
   meta.name = 'urn:adobe:aue:config:extensions';
 
  if (window.location.href.includes('cmstg')==true){
-   meta.content =   'https://experience.adobe.com/solutions/livecycle-datasource-ui-service/resources/universal_editor?livecycle-datasource-ui-service_version=PR-15-055e7e89563e3c68cd65d3d875f5ed9cb90d4e17&shell_domain=author.adobeaemcloud.com';
+   meta.content =   'https://experience.adobe.com/solutions/livecycle-datasource-ui-service/resources/universal_editor?livecycle-datasource-ui-service_version=PR-22-5333d4ffb6dfe70725f6c7c4e60fc8110c7ee13c&shell_domain=author.adobeaemcloud.com';
  }
  else{
-  meta.content =   'https://experience.adobe.com/solutions/livecycle-datasource-ui-service/resources/universal_editor?livecycle-datasource-ui-service_version=PR-15-055e7e89563e3c68cd65d3d875f5ed9cb90d4e17&shell_domain=author.adobeaemcloud.com';
+  meta.content =   'https://experience.adobe.com/solutions/livecycle-datasource-ui-service/resources/universal_editor?livecycle-datasource-ui-service_version=PR-22-5333d4ffb6dfe70725f6c7c4e60fc8110c7ee13c&shell_domain=author.adobeaemcloud.com';
  }
  
 
